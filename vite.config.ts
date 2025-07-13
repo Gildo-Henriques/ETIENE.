f251a673e78af081ae0df51afbc34ed9caf80b2c
Gildo-Henriques <gildohenriques520@gmail.com>
2025-07-13 22:40:42 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Corrige __dirname em ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +13,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets')
-    }
-  }
-})
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
